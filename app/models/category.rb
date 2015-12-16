d205a41e2907d04856ee0948e718fef378929686
Tamara Temple <tamouse@gmail.com>
2015-12-15 22:08:01 -0600

    Modify category (and tests) for single root

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,6 +5,28 @@ class Category < ActiveRecord::Base
   scope :with_children, ->() { joins(:children).distinct }
   scope :top_level, ->() { where(parent_id: nil) }
 
+  before_save :ensure_one_root
+
+  ##
+  # CLASS METHODS
+  ##
+
+  def self.root
+    self.top_level.first
+  end
+
+  def self.make_root(other)
+    self.transaction do
+      old_root = self.root
+      old_root.update_column(:parent_id, other.id)
+      other.update_column(:parent_id, nil)
+    end
+  end
+
+  ##
+  # INSTANCE METHODS
+  ##
+
   def siblings
     if parent
       parent.children.where.not(id: self.id)
@@ -15,6 +37,25 @@ class Category < ActiveRecord::Base
 
   def has_parent?
     self.parent.present?
+  end
+
+  def is_leaf?
+    self.children.empty?
+  end
+
+  def is_root?
+    self.parent.nil?
+  end
+
+  def make_root
+    self.class.make_root(self)
+  end
+
+  private
+
+  def ensure_one_root
+    return false if self == self.class.root
+    self.parent = self.class.root if parent_id.nil?
   end
 
 end
